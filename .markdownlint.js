bedef2ffca20bef9d9a83a4872c0452f2a897483
Andri Alexandrou <andri.j.alexandrou@gmail.com>
2022-08-03 13:25:24 +0300

    feat: adds CI steps for linting and testing (#3)

---
+++ .markdownlint.js
@@ -0,0 +1,12 @@
+const options = {
+    config: {
+        "default": false,
+        "heading-increment": true,
+        "no-alt-text": true,
+        "single-h1": true,
+        "no-emphasis-as-header": true,
+        "first-line-heading": true
+    }
+}
+
+module.exports = options.config
