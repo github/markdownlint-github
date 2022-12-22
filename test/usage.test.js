c9941262a30bd98bcac8a6ee75b65596561ed429
Kate Higa <khiga8@github.com>
2022-12-22 12:57:46 -0500

    Fix test

--- test/usage.test.js
+++ test/usage.test.js
@@ -17,6 +17,7 @@ describe("usage", () => {
         "no-space-in-links": false,
         "single-h1": true,
         "no-emphasis-as-header": true,
+        "no-heading-increment": true,
         "no-generic-link-text": true,
         "ul-style": true,
         default: true,
